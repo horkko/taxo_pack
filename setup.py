64c215194fc8deb453f2db937b4e33e5e7fa8554
maufrais <maufrais@pasteur.fr>
2015-10-02 14:54:55 +0200

    T: add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name = "taxo_pack",
+    version = "2.0",
+    description = (""),
+    license='',
+    author = "Corinne Maufrais",
+    author_email = "corinne.maufrais@pasteur.fr",
+    url = "",
+    package_dir={'': 'src'},
+    packages=[''],
+    install_requires = ['golden >= 3.0, 
+	'bsddb3 >=6.1.0', 
+],
+) 
