049989f6b6edfbc05bd83b71070e5b9a1f1bfd6a
Corinne Maufrais <maufrais@pasteur.fr>
2015-10-08 17:58:36 +0200

    T: distribution preparation C: add setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name = "taxo_pack",
     version = "2.0",
@@ -6,10 +9,9 @@ setup(name = "taxo_pack",
     license='',
     author = "Corinne Maufrais",
     author_email = "corinne.maufrais@pasteur.fr",
-    url = "",
     package_dir={'': 'src'},
-    packages=[''],
-    install_requires = ['golden >= 3.0, 
+    install_requires = ['golden >= 3.0', 
 	'bsddb3 >=6.1.0', 
+	'KronaTools >= 2.6', 	
 ],
 ) 
